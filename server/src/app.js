da535ad85c57fc785da6b8b8050ea1dd301c4e31
ruthenicgull <muhammedhisham2003@gmail.com>
2024-10-20 20:14:02 +0530

    Revert "Revert "fixed state and patient age""
    This reverts commit 0896eeb770e4bcf76825d3596c177c7bbced2a97.

--- server/src/app.js
+++ server/src/app.js
@@ -54,7 +54,7 @@ const buildQuery = (filters) => {
           items: {
             $elemMatch: {
               archetype_node_id: "at0010",
-              "value.value": filters.reasonForEncounter,
+              "value.value": filters.reasonForEncounter.toUpperCase(),
             },
           },
         },
@@ -86,7 +86,7 @@ const buildQuery = (filters) => {
           items: {
             $elemMatch: {
               archetype_node_id: "at0028",
-              "value.value": filters.state,
+              "value.value": filters.state.toUpperCase(),
             },
           },
         },
@@ -234,8 +234,6 @@ const buildQuery = (filters) => {
     query["composer.name"] = filters.committerName;
   }
 
-  console.log(query["content"].$elemMatch.$and[0]);
-
   return query;
 };
 
@@ -248,7 +246,6 @@ app.post("/api/filter", async (req, res) => {
     // Directly query the MongoDB collection using Mongoose
     const db = mongoose.connection.db;
     const compositions = await db.collection("documents").find(query).toArray();
-    console.log(compositions);
 
     res.status(200).json(compositions);
   } catch (err) {
