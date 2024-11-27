fb0d242116b67579c5d5b2ca4e3509a155629954
ruthenicgull <muhammedhisham2003@gmail.com>
2024-11-27 14:04:52 +0530

    added radiotherpy ehrs

--- server/src/modules/test/test.js
+++ server/src/modules/test/test.js
@@ -1,6 +1,6 @@
 import EHRFlattener from "../EHRFlattener.js";
-import ehr1 from "./sampleEHR1.json" with { type: "json" };
-import ehr2 from "./sampleEHR2.json" with { type: "json" };
+// import ehr1 from "./sampleEHR1.json" with { type: "json" };
+// import ehr2 from "./sampleEHR2.json" with { type: "json" };
 
 const ehrFlattener = new EHRFlattener();
 
